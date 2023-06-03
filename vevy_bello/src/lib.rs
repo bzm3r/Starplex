752601baf85d071953f1e9f3084dfa2a929299e5
Brian Merchant <bhmerchant@gmail.com>
2023-06-02 18:47:15 -0700

    Use main branch of bevy (#2)
    * Switched to Bevy dev branch, and compilation + running works, but nothing is being rendered sadly
    
    * fixed rendering

--- vevy_bello/src/lib.rs
+++ vevy_bello/src/lib.rs
@@ -44,10 +44,28 @@ pub fn render_scenes(
 impl Plugin for VelloPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugin(ExtractComponentPlugin::<VelloScene>::default());
+        // .add_plugins(DefaultPlugins.set(bevy::log::LogPlugin {
+        //     // Uncomment this to override the default log settings:
+        //     // level: bevy::log::Level::TRACE,
+        //     // filter: "wgpu=warn,bevy_ecs=info".to_string(),
+        //     ..default()
+        // }));
 
-        let Ok(render_app) = app.get_sub_app_mut(RenderApp) else { return };
-        render_app.init_resource::<VelloRenderer>();
+        // We need to get the render app from the main app
+        let Ok(render_app) = app.get_sub_app_mut(RenderApp) else {
+            return;
+        };
         // This should probably use the render graph, but working out the dependencies there is awkward
         render_app.add_systems(Render, render_scenes.in_set(RenderSet::Render));
+    }
+
+    fn finish(&self, app: &mut App) {
+        // We need to get the render app from the main app
+        let Ok(render_app) = app.get_sub_app_mut(RenderApp) else {
+            return;
+        };
+
+        // Initialize the Vello Renderer
+        render_app.init_resource::<VelloRenderer>();
     }
 }
