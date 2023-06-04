66eb4182da26bf2135714c0ae953e1929d12d722
Brian Merchant <bhmerchant@gmail.com>
2023-06-03 17:31:28 -0700

    Clippy + changes due to Vello update

--- vevy_bello/src/renderer.rs
+++ vevy_bello/src/renderer.rs
@@ -1,6 +1,6 @@
 use bevy::{
     prelude::{FromWorld, Resource, World},
-    render::renderer::RenderDevice,
+    render::renderer::{RenderDevice, RenderQueue},
 };
 use vello::{Renderer, RendererOptions};
 
@@ -10,11 +10,14 @@ pub struct VelloRenderer(pub Renderer);
 impl FromWorld for VelloRenderer {
     fn from_world(world: &mut World) -> Self {
         let device = world.get_resource::<RenderDevice>().unwrap();
+        let queue = world.get_resource::<RenderQueue>().unwrap();
+
         VelloRenderer(
             Renderer::new(
                 device.wgpu_device(),
                 &RendererOptions {
                     surface_format: None,
+                    timestamp_period: queue.0.get_timestamp_period(),
                 },
             )
             .unwrap(),
